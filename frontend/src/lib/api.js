a17759e05b67ae26e5b12727bc9774267c53674f
Administrator <souravverma1582000@gmail.com>
2026-04-27 02:24:03 +0100

    add more files and did changed in requi

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE = "http://127.0.0.1:8000/api";
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000';
 export const api = axios.create({
   baseURL: API_BASE,
   headers: { "Content-Type": "application/json" },
